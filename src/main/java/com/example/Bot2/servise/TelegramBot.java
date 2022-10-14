59d7430f7ff2533f42c4d1da5bef25fda4e5fdfc
MykolaDiachuk <diachuk.mykola2005@gmail.com>
2022-10-14 16:42:05 +0300

    y

--- src/main/java/com/example/Bot2/servise/TelegramBot.java
+++ src/main/java/com/example/Bot2/servise/TelegramBot.java
@@ -4,15 +4,20 @@ package com.example.Bot2.servise;
 import com.example.Bot2.Config.BotConfig;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendDocument;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
-import org.telegram.telegrambots.meta.api.objects.Message;
-import org.telegram.telegrambots.meta.api.objects.MessageEntity;
-import org.telegram.telegrambots.meta.api.objects.Update;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendPhoto;
+import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 
@@ -51,9 +56,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         if (update.hasMessage() && update.getMessage().hasText()) {
             SendMessage message = new SendMessage();
 
+            String name = update.getMessage().getChat().getFirstName();
             Message updateMessage = update.getMessage();
             long chatId = update.getMessage().getChatId();
-            String name = update.getMessage().getChat().getFirstName();
+            String fileId = updateMessage.getDocument().getFileId();
+
 
             switch (updateMessage.getText()) {
                 case "/start":
@@ -71,15 +78,18 @@ public class TelegramBot extends TelegramLongPollingBot {
                     break;
                 default:
                     if (updateMessage.hasText()) {
-                        preLibrarian(updateMessage);
-                        message.setReplyMarkup(getSaveMenu());
+                        if(!folder.contains(message.getText())){
+                            preLibrarian(updateMessage);
+                        }else nameOfFolder = updateMessage.getText();
 
-                    } else if (updateMessage.hasAnimation() || updateMessage.hasAudio() ||
-                            updateMessage.hasContact() || updateMessage.hasDocument() || updateMessage.hasPhoto()
-                            || updateMessage.hasVideo() || updateMessage.hasVoice()) {
+                        sendMsg(chatId, "Що зберегти (файл, ссилка, фото...)");
 
-                        librarian(updateMessage);
-                        findObject(chatId,result);
+                    } else if (updateMessage.hasDocument()) {
+                        // sendDoc(chatId,;
+                        if(library.containsKey(nameOfFolder)){
+                            librarian2(updateMessage);
+                        }else librarian(updateMessage);
+                        // findObject(chatId,result);
                     }
             }
         }
@@ -137,51 +147,68 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
 
-    private ReplyKeyboardMarkup getSaveMenu() {
-
-        KeyboardRow row1 = new KeyboardRow();
-        row1.add("Що зберегти (файл, ссилка, фото...)");
-        List<KeyboardRow> rows = new ArrayList<>();
-        rows.add(row1);
-        replyKeyboardMarkup.setKeyboard(rows);
-
-        return replyKeyboardMarkup;
-    }
-
-    //запис у першу мапу. потрібно для того, щоб по chatId отримати kay для другої мапи
+    //запис у перший лист. потрібно для того, щоб можна було знайти kеy для другої мапи.
     public void preLibrarian(Message message) {
+
         folder.add(message.getText());
         nameOfFolder = message.getText();
     }
 
-    // запис у другу головну мапу
+    // запис у  головну мапу
     public void librarian(Message message) {
         result.add(message);
         library.put(nameOfFolder, result);
+        someMessage = message;
     }
 
-    // пошук об'єктів, повертає список обєктів
+    public void librarian2(Message message) {
+        List<Message> addNewObject = library.get(nameOfFolder);
+        addNewObject.add(message);
+        library.put(nameOfFolder,addNewObject);
+        someMessage = message;
+    }
+
+    // пошук у першому лисі
     public void preFindObject(Long chatId) {
         sendObject(chatId, folder);
 
     }
 
-    public void findObject(Long chatId, List<Message> result) {
-        int a = result.size();
-        for (int i = 0; i!=a; i++){
-            message.setChatId(chatId);
-            message.setEntities((List<MessageEntity>) result.get(i));
-            try {
-                execute(message);
-            } catch (TelegramApiException e) {
 
-            }
+    /* public void findObject(Long chatId) {
+         List<Message> object = library.get(nameOfFolder);
+         int a = object.size();
+         for (int i = 0; i!=a; i++){
+             message.setChatId(chatId);
+
+
+             try {
+                 execute(message);
+             } catch (TelegramApiException e) {
+
+             }
+         }
+
+     }*/
+    public void sendDoc(Long chatId, File sendFile) {
+        InputFile inputFile = new InputFile(String.valueOf(sendFile));
+        SendDocument sendDocument = new SendDocument();
+        sendDocument.setChatId(chatId);
+        sendDocument.setDocument(inputFile);
+        try {
+            execute(sendDocument);
+        } catch (TelegramApiException e) {
+            throw new RuntimeException(e);
         }
-
     }
-
-
     //userId -> massage
     // userid:metemateka -> List<messageId>
-    //
+
+
 }
+
+
+
+
+
+
