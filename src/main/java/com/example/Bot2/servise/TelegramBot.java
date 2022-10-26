c41571c2aad494642248d4330ca4acb666020bfa
MykolaDiachuk <diachuk.mykola2005@gmail.com>
2022-10-26 19:46:51 +0300

    2

--- src/main/java/com/example/Bot2/servise/TelegramBot.java
+++ src/main/java/com/example/Bot2/servise/TelegramBot.java
@@ -3,7 +3,6 @@ package com.example.Bot2.servise;
 
 import com.example.Bot2.Config.BotConfig;
 import lombok.SneakyThrows;
-import org.apache.commons.io.FileUtils;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.GetFile;
@@ -14,17 +13,18 @@ import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.*;
 import org.telegram.telegrambots.meta.api.objects.File;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class TelegramBot extends TelegramLongPollingBot {
     final BotConfig config;
-    private Keyboard keyboard = new Keyboard();
-    private Library library = new Library();
+ private final Serialization serial = new Serialization();
+    private final Keyboard keyboard = new Keyboard();
+    private final Library library = new Library();
 
 
     public TelegramBot(BotConfig config) {
@@ -43,16 +43,20 @@ public class TelegramBot extends TelegramLongPollingBot {
 
 
     SendMessage message = new SendMessage();
+    long chatId=0;
 
+    Message updateMsg = null;
 
     @SneakyThrows
     @Override
     public void onUpdateReceived(Update update) {
+        this.updateMsg=update.getMessage();
 
         if (update.hasMessage()) {
             String name = update.getMessage().getChat().getFirstName();
             Message updateMessage = update.getMessage();
-            long chatId = update.getMessage().getChatId();
+            this.chatId = update.getMessage().getChatId();
+
             String text = updateMessage.getText() == null ? "" : updateMessage.getText();
 
 
@@ -80,9 +84,9 @@ public class TelegramBot extends TelegramLongPollingBot {
                             messageEntity.setUrl(updateMessage.getText());
                             messageEntity.setOffset(0);
                             messageEntity.setLength(4);
-                            List<MessageEntity> entities = new ArrayList<>();
-                            entities.add(messageEntity);
-
+                            //serial.serialization(messageEntity);
+                            //java.io.File file = ResourceUtils.getFile("C:\\Games\\Bot2\\src\\main\\resources\\new.txt");
+                            saveObject(messageEntity);
 
                         }else if (!library.folder.contains(updateMessage.getText())) { // якщо у листі немає такого обєкту
                             library.preLibrarian(updateMessage);
@@ -110,9 +114,10 @@ public class TelegramBot extends TelegramLongPollingBot {
                         } catch (TelegramApiException e) {
                             throw new RuntimeException(e);
                         }
-                        if (library.libraryOfMessage.containsKey(library.nameOfFolder)) { // якщо такий ключ вже є
-                            library.librarian2(file);
-                        } else library.librarian(file);
+                       // serial.serialization(file);
+                        //java.io.File file1 = ResourceUtils.getFile("C:\\Games\\Bot2\\src\\main\\resources\\new.txt");
+                         saveObject(file);
+
 
                     } else if (updateMessage.hasPhoto()){
                         sendMsg(chatId,"photo");
@@ -122,7 +127,14 @@ public class TelegramBot extends TelegramLongPollingBot {
             CallbackQuery callbackQuery = update.getCallbackQuery();
             String data = callbackQuery.getData();
             library.nameOfFolder = data;
-            findObject(update.getCallbackQuery().getMessage().getChatId(), data);
+            //findObject(update.getCallbackQuery().getMessage().getChatId(), data, update );
+            List object = library.libraryOfMessage.get(library.nameOfFolder);
+            int a = object.size();
+            for (int i = 0; i != a; i++) {
+                send(chatId,object.get(i));
+
+            }
+            sendMsg(chatId,"d");
         }
     }
 
@@ -131,7 +143,11 @@ public class TelegramBot extends TelegramLongPollingBot {
         String answer = "Hi, " + name + ", nice to meet you!";
         sendMsg(chatId, answer);
     }
-
+public void saveObject(Object file){
+    if (library.libraryOfMessage.containsKey(library.nameOfFolder)) { // якщо такий ключ вже є
+        library.librarian2(file);
+    } else library.librarian(file);
+}
     //Надсилання списку
     private void sendList(Long chatId, List<String> messageToSand) {
 
@@ -161,17 +177,19 @@ public class TelegramBot extends TelegramLongPollingBot {
 
         }
     }
-    /*  private void sendUlr(long chatId, ){
+     private void send(long chatId, List<MessageEntity> entities ){
+
         SendMessage message = new SendMessage();
         message.setChatId(String.valueOf(chatId));
         message.setEntities(entities);
-        message.setText(updateMessage.getText());
+        message.setText(entities.toString());
+
         try {
             execute(message);
         } catch (TelegramApiException e) {
             e.printStackTrace();
         }
-    }*/
+    }
 
     // не звертай увагу, ще не дописано
     public void sendKeyboard(Long chatId, String text, InlineKeyboardMarkup inlineKeyboardMarkup) {
@@ -188,12 +206,12 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     // мало б надсилати документи, але setDocument() у якості аргументу хоче InputFile, який я не моду отримати просто так
-    public void sendDoc(Long chatId, java.io.File file, String fileName) {
+    public void send(Long chatId, Object file) {
 
         SendDocument sendDocument = new SendDocument();
-        sendDocument.setCaption(fileName);
+        sendDocument.setCaption(((java.io.File)file).getName());
         sendDocument.setChatId(chatId.toString());
-        sendDocument.setDocument(new InputFile(file));
+        sendDocument.setDocument(new InputFile((java.io.File)file));
         try {
             execute(sendDocument);
         } catch (TelegramApiException e) {
@@ -209,17 +227,23 @@ public class TelegramBot extends TelegramLongPollingBot {
     }
 
     //пробував виводити обєкти листа з документами, але для цього потрібно їх надсилати користувачу.
-    public void findObject(Long chatId, String fileName) {
-        List<java.io.File> object = library.libraryOfMessage.get(library.nameOfFolder);
+   /* public void findObject(Long chatId, String fileName, Update update) throws IOException, ClassNotFoundException {
+        List object = library.libraryOfMessage.get(library.nameOfFolder);
         int a = object.size();
         for (int i = 0; i != a; i++) {
-            sendDoc(chatId, object.get(i), fileName);
+            if(update.getMessage().hasDocument()){
+                send(chatId, (java.io.File)object.get(i), fileName);
+            } else if (update.getMessage().hasEntities()){
+                List<MessageEntity>entities=new ArrayList<>();
+                entities.add((MessageEntity) object.get(i));
+                send(chatId, entities);
+            }
+
         }
 
-    }
+    }*/
 
-    //userId -> massage
-    // userid:metemateka -> List<messageId>
+
 
 
 }
